93b61ff9739f258795f6958a8fa1cae8c40976d8
sandippan52 <sandippan52@gmail.com>
2026-01-10 04:11:15 +0530

    delete previous cors completely

--- backend/server.js
+++ backend/server.js
@@ -39,21 +39,7 @@ app.use(
   })
 );
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (Postman, curl)
-      if (!origin) return callback(null, true);
 
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
 
 
 
