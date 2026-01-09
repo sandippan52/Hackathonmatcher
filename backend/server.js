81cc4499397793c44918f82239418009ec8100a0
sandippan52 <sandippan52@gmail.com>
2026-01-10 04:03:51 +0530

    wrong axios url fixed

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.set("trust proxy", 1);
 
 app.use(
   cors({
-    origin: "https://hackathon-matcher.vercel.app",
+    origin: "https://hackathonmatcher.vercel.app",
     credentials: true,
   })
 );
